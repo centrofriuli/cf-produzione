dc62c25dd4de9bee6f1ad2311780f9aeaea28dba
informatici@centrofriuli.tu <Inf0rm4t1c1>
2022-10-10 17:43:48 +0200

    NEW: aggiunti grafici pdf

--- app/Http/Controllers/TrattativaController.php
+++ app/Http/Controllers/TrattativaController.php
@@ -95,8 +95,13 @@ class TrattativaController extends Controller
             }
         }
 
+        //configurazione grafico
+        //https://quickchart.io/chart-maker/edit/zm-27bd53e9-5bc9-45ed-9cbb-d57d7266891d
+        $pieChartUrl = "https://quickchart.io/chart/render/zm-f2c09825-2aa9-4631-acad-90735d51fba9"."?data1=".count($datiBisogniNonGestite).",".count($datiBisogniGestiteDaMigliorare).",".count($datiBisogniGestiteBene).",".count($datiBisogniNonGestite);
+        $insuranceIndexUrl = "https://quickchart.io/chart/render/zm-c5f19d75-17af-4aa2-a3dd-89f661189aa0"."?data1=".count($datiBisogniGestiteBene)."&data2=".count($datiBisogniGestiteDaMigliorare)."&data3=".count($datiBisogniNonGestite)+count($datiBisogniNonConsiderate);
+        $qualityIndexUrl = "https://quickchart.io/chart/render/zm-12d08832-9f33-42a7-89f2-8128803df631"."?data1=".count($datiBisogniGestiteBene).",".count($datiBisogniGestiteDaMigliorare);
 
-        $pdf = Pdf::loadView('pdf.trattativa', array("datiBisogniNonGestite" => $datiBisogniNonGestite, "datiBisogniGestiteDaMigliorare" => $datiBisogniGestiteDaMigliorare, "datiBisogniNonConsiderate" => $datiBisogniNonConsiderate, "datiBisogniGestiteBene" => $datiBisogniGestiteBene));
+        $pdf = Pdf::loadView('pdf.trattativa', array("datiBisogniNonGestite" => $datiBisogniNonGestite, "datiBisogniGestiteDaMigliorare" => $datiBisogniGestiteDaMigliorare, "datiBisogniNonConsiderate" => $datiBisogniNonConsiderate, "datiBisogniGestiteBene" => $datiBisogniGestiteBene, "pieChartUrl" => $pieChartUrl, 'insuranceIndexUrl' => $insuranceIndexUrl, "qualityIndexUrl" => $qualityIndexUrl));
 
         return $pdf->stream('trattativa' . $idBisogno . '.pdf');
 
